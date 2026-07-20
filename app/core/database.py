91d2556a645570e4fa468e913e0fc8d69be836e8
xDennis20 <dennis_6@outlook.com>
2026-07-19 23:52:04 -0500

    "feat: cambios de importaciones en database

--- app/core/database.py
+++ app/core/database.py
@@ -1,14 +1,5 @@
 from sqlmodel import create_engine, Session, SQLModel
 from app.core.config import DATABASE_URL
-from app.models.restaurante import Restaurante
-from app.models.usuario import Usuario
-from app.models.pedido import Pedido, DetallePedido
-from app.models.mesa import Mesa
-from app.models.plato import Plato
-from app.models.categoria import Categoria
-from app.models.rol import Rol
-from app.models.factura import Factura
-
 
 engine = create_engine(DATABASE_URL, echo=True)
 
