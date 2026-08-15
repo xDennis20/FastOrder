78805a5a18110ea9769a09f2efd6a9e16fd950e3
xDennis20 <dennis_6@outlook.com>
2026-08-15 14:09:29 -0500

    feat: Nuevo endpoint obtener una mesa por id, y en proceso del siguiente endopoint cambiar de estados

--- app/api/v1/mesas.py
+++ app/api/v1/mesas.py
@@ -1,8 +1,11 @@
+from http.cookiejar import cut_port_re
+
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlmodel import select, Session
-from app.models.mesa import MesaCreate, MesaRead, Mesa, MesaVincular
+from app.models.mesa import MesaCreate, MesaRead, Mesa, MesaVincular, EstadosValidos, MesaEstadoUpdate
 from app.api.deps import get_current_user, get_session
+from app.models.pedido import Pedido
 
 router = APIRouter(prefix="/mesas", tags=["mesas"])
 
@@ -65,11 +68,25 @@ def vincular_mesas(mesa_id: int,
         db.rollback()
         raise HTTPException(status_code=500, detail="Error interno al modificar el objeto en la base de datos")
 
+@router.patch("/{mesa_id}/estado", response_description="Cambio de estado de la mesa exitoso")
+def mesa_cambiar_estado(mesa_id: int, estado: MesaEstadoUpdate,current_user: dict = Depends(get_current_user), db: Session = Depends(get_session)):
+    consulta_mesa = select(Mesa).where(Mesa.restaurante_id == current_user.get("restaurante_id"), Mesa.id == mesa_id)
+    consulta_pedidos_activos = select(Pedido).where(Pedido.restaurante_id == current_user.get("restaurante_id", Pedido.mesa_id == mesa_id, Pedido.estado.in_(["Pendiente", "En preparación", "Entregado"])))
+
+
 @router.get("/", response_model=list[MesaRead])
 def obtener_mesas(current_user: dict = Depends(get_current_user), db: Session = Depends(get_session)):
     consulta = select(Mesa).where(Mesa.restaurante_id == current_user.get("restaurante_id"))
     mesas_items = db.exec(consulta).all()
     return mesas_items
+
+@router.get("/{mesa_id}", response_model=MesaRead)
+def obtener_mesa(mesa_id: int, current_user: dict = Depends(get_current_user), db: Session = Depends(get_session)):
+    consulta = select(Mesa).where(Mesa.restaurante_id == current_user.get("restaurante_id"), Mesa.id == mesa_id)
+    mesa_item = db.exec(consulta).one()
+    if mesa_item is None:
+        raise HTTPException(status_code=404, detail="Mesa no encontrada")
+    return mesa_item
 
 @router.delete("/{mesa_id}", status_code=status.HTTP_200_OK)
 def eliminar_mesa(mesa_id: int, current_user: dict = Depends(get_current_user), db: Session = Depends(get_session)) -> dict:
