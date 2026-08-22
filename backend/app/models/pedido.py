394565c0baad24740cedbddfecca903f0f11eed4
xDennis20 <dennis_6@outlook.com>
2026-08-22 17:03:18 -0500

    "feat: Nuevos esquemas y modificaciones en el modelo ORM de DetallePedidos"

--- backend/app/models/pedido.py
+++ backend/app/models/pedido.py
@@ -1,17 +1,47 @@
+from datetime import datetime, UTC
+from decimal import Decimal
 from typing import TYPE_CHECKING,  Optional
 from sqlmodel import SQLModel, Field, Relationship
-from datetime import datetime, UTC
+from enum import Enum
 
 if TYPE_CHECKING:
     from app.models.usuario import Usuario
-    from app.models.plato import Plato
+    from app.models.plato import Plato, PlatoRead
     from app.models.factura import Factura
     from app.models.mesa import Mesa
     from app.models.restaurante import Restaurante
 
+class EstadosValidosPedidos(str, Enum):
+    PENDIENTE = "Pendiente"
+    EN_PREPARACION = "En preparacion"
+    LISTO = "Listo"
+    SERVIDO = "Servido"
+    PAGADO = "Pagado"
+    CANCELADO = "Cancelado"
+
+class EstadosValidosDetalles(str, Enum):
+    PENDIENTE = "Pendiente"
+    EN_PREPARACION = "En preparacion"
+    LISTO = "Listo"
+    CANCELADO = "Cancelado"
+
+class PedidoBase(SQLModel):
+    estado : EstadosValidosPedidos = EstadosValidosPedidos.PENDIENTE
+    mesa_id : int | None
+    mesero_id : int | None
+    fecha_creacion : datetime = Field(default_factory=lambda: datetime.now(UTC))
+
+class DetallePedidoBase(SQLModel):
+    pedido_id: int | None
+    plato_id: int | None
+    precio_unitario: Decimal | None
+    cantidad: int | None = 1
+    notas: str | None
+    estado: EstadosValidosDetalles = EstadosValidosDetalles.PENDIENTE
+
 class Pedido(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True, index=True)
-    estado: str = Field(default="Pendiente", nullable=False)
+    estado: str = Field(default=EstadosValidosPedidos.PENDIENTE, nullable=False)
 
     mesa_id: int | None = Field(default=None, foreign_key="mesa.id")
     mesero_id: int | None = Field(default=None, foreign_key="usuario.id")
@@ -30,11 +60,13 @@ class DetallePedido(SQLModel, table=True):
     pedido_id: int | None = Field(default=None, foreign_key="pedido.id")
     plato_id: int | None = Field(default=None, foreign_key="plato.id")
     cantidad: int = Field(default=1)
-    notas: str | None = Field(max_length=100)
-    estado: str = Field(default="Pendiente", nullable=False)
+    precio_unitario: Decimal | None = Field(default=None)
+    notas: str | None = Field(default=None,max_length=100)
+    estado: str = Field(default=EstadosValidosDetalles.PENDIENTE, nullable=False)
 
     pedido: Optional["Pedido"] = Relationship(back_populates="detalles")
     plato: Optional["Plato"] = Relationship(back_populates="detalles_pedido")
+
 
 class DetallePedidoCreate(SQLModel):
     plato_id: int
@@ -45,8 +77,27 @@ class PedidoCreate(SQLModel):
     mesa_id: int
     detalles: list[DetallePedidoCreate]
 
+class DetallePedidoRead(DetallePedidoBase):
+    id: int
+    plato: PlatoRead | None = None
+
+class PedidoRead(PedidoBase):
+    id: int
+    detalles : list[DetallePedidoRead] = []
+    restaurante_id: int
+
+class PedidoPagination(SQLModel):
+    items: list[PedidoRead] = []
+    total: int
+    limit: int
+    offset: int
+    pagina: int
+    total_paginas: int
+    tiene_siguiente: bool
+    tiene_anterior: bool
+
 from app.models.usuario import Usuario
-from app.models.plato import Plato
+from app.models.plato import Plato, PlatoRead
 from app.models.factura import Factura
 from app.models.mesa import Mesa
 from app.models.restaurante import Restaurante
