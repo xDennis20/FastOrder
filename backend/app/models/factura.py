2c3db4b7d24277d3cad58bb3163cbb2c800a7bfe
xDennis20 <dennis_6@outlook.com>
2026-08-28 12:54:01 -0500

    feat: Nuevos esquemas de Factura

--- backend/app/models/factura.py
+++ backend/app/models/factura.py
@@ -1,18 +1,39 @@
-from typing import TYPE_CHECKING, Optional
+from enum import Enum
+from typing import TYPE_CHECKING, Optional, Literal
 from decimal import Decimal
 from sqlmodel import SQLModel, Field, Relationship
 
 if TYPE_CHECKING:
     from app.models.pedido import Pedido
 
+class TiposPagosValidos(Enum):
+    EFECTIVO = "Efectivo"
+    TRANSFERENCIA = "Transferencia"
+
+class FacturaBase(SQLModel):
+    pedido_id: int | None
+    tipo_pago: Literal[TiposPagosValidos.EFECTIVO, TiposPagosValidos.TRANSFERENCIA] | None = None
+    comprobante_img_url: str | None
+
 class Factura(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True, index=True)
-    pedido_id: int | None = Field(default=None, foreign_key="pedido.id")
-    tipo_pago: str = Field(default="Efectivo", nullable=False)
+    pedido_id: int | None = Field(foreign_key="pedido.id", unique=True, nullable=False, index=True)
+    tipo_pago: str = Field(default=TiposPagosValidos.EFECTIVO, nullable=False)
     comprobante_img_url: str | None = Field(default=None, nullable=True)
     total: Decimal = Field(default=0, max_digits=8, decimal_places=2)
 
     pedido: Optional["Pedido"] = Relationship(back_populates="factura")
+
+class FacturaCreate(SQLModel):
+    tipo_pago: Literal[TiposPagosValidos.EFECTIVO, TiposPagosValidos.TRANSFERENCIA]
+    comprobante_img_url: str | None = None
+
+class FacturaRead(SQLModel):
+    id: int
+    pedido_id: int
+    tipo_pago: TiposPagosValidos
+    comprobante_img_url: str | None
+    total: Decimal
 
 from app.models.pedido import Pedido
 Factura.model_rebuild()
