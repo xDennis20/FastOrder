914bbf203623cad2cf5d2b14c795c92b6b7788c8
xDennis20 <dennis_6@outlook.com>
2026-09-02 14:46:27 -0500

    feat(categorias): Mejoramiento del CRUD completo con multi-tenancy y cache en memoria

--- backend/app/api/v1/categorias.py
+++ backend/app/api/v1/categorias.py
@@ -1,26 +1,40 @@
 import time
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import selectinload
 from sqlmodel import Session, select
-from app.api.deps import get_session
-from app.service.restaurante import verificar_restaurante
-from app.models.categoria import Categoria, CategoriaCreate, CategoriaRead, CategoriaWithPlatos
+from app.api.deps import get_session, VerificarRol
+from app.api.v1.auth.schemas import TokenData
+from app.models.usuario import RolesValidos
+from app.models.categoria import (Categoria, CategoriaCreate, CategoriaRead,
+                                  CategoriaWithPlatos, CategoriaUpdate)
 
 router = APIRouter(prefix="/categorias", tags=["categorias"])
 
-CACHE_CATEGORIAS = None
-CACHE_EXPIRATION_TIME = 0
+CACHE_CATEGORIAS: dict[tuple[int,bool], dict[str, list[CategoriaWithPlatos] | float]] = {}
 CACHE_DURATION_SECONDS = 60
 
-@router.post("/", response_model=CategoriaRead)
-def crear_categoria(categoria_in: CategoriaCreate, db: Session = Depends(get_session)):
-    restaurante_obj = verificar_restaurante(categoria_in.restaurante_id, db)
+@router.post("/", response_model=CategoriaRead, status_code=status.HTTP_201_CREATED)
+def crear_categoria(categoria_in: CategoriaCreate,
+                    current_user: TokenData = Depends(VerificarRol([RolesValidos.DUENO])),
+                    db: Session = Depends(get_session)):
+    consulta_categoria_exc = (select(Categoria)
+                              .where(Categoria.nombre == categoria_in.nombre,
+                                     Categoria.restaurante_id == current_user.restaurante_id))
+
+    categoria_exc_obj = db.exec(consulta_categoria_exc).first()
+
+    if categoria_exc_obj is not None:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="Categoria ya existente")
+
     try:
         categoria_nueva = Categoria.model_validate(categoria_in)
-        categoria_nueva.restaurante_id = restaurante_obj.id
+        categoria_nueva.restaurante_id = current_user.restaurante_id
         db.add(categoria_nueva)
         db.commit()
+        CACHE_CATEGORIAS.pop((current_user.restaurante_id, False), None)
+        CACHE_CATEGORIAS.pop((current_user.restaurante_id, True), None)
         db.refresh(categoria_nueva)
         return categoria_nueva
     except SQLAlchemyError:
@@ -28,16 +42,121 @@ def crear_categoria(categoria_in: CategoriaCreate, db: Session = Depends(get_ses
         raise HTTPException(status_code=500, detail="Error interno al guardar en la base de datos")
 
 @router.get("", response_model=list[CategoriaWithPlatos])
-def obtener_categorias(db: Session = Depends(get_session)):
-    global CACHE_CATEGORIAS, CACHE_EXPIRATION_TIME
+def obtener_categorias(current_user: TokenData = Depends(VerificarRol([RolesValidos.DUENO,RolesValidos.MESERO])),
+                       db: Session = Depends(get_session)):
+    activo = True if current_user.rol == RolesValidos.MESERO else False
+    clave_cache = (current_user.restaurante_id, activo)
 
-    if CACHE_CATEGORIAS is not None and time.time() < CACHE_EXPIRATION_TIME:
-        return CACHE_CATEGORIAS
+    cache = CACHE_CATEGORIAS.get(clave_cache)
 
-    statement = select(Categoria).options(selectinload(Categoria.platos))
+    if cache and time.time() < cache.get("expira_en"):
+        return cache.get("items")
+
+    statement = (select(Categoria).options(selectinload(Categoria.platos))
+                 .where(Categoria.restaurante_id == current_user.restaurante_id)
+                 .order_by(Categoria.nombre.asc()))
+
+    if activo:
+        statement = statement.where(Categoria.activo == True)
+
     categoria_items = db.exec(statement).all()
 
-    CACHE_CATEGORIAS = categoria_items
-    CACHE_EXPIRATION_TIME = time.time() + CACHE_DURATION_SECONDS
+    CACHE_CATEGORIAS[clave_cache] = {"items": categoria_items,
+                                                     "expira_en": time.time() + CACHE_DURATION_SECONDS}
 
     return categoria_items
+
+@router.get("/{categoria_id}", response_model=CategoriaWithPlatos)
+def obtener_categoria(categoria_id: int,
+                      current_user: TokenData = Depends(VerificarRol([RolesValidos.MESERO, RolesValidos.DUENO, RolesValidos.CAJA])),
+                      db: Session = Depends(get_session)):
+    consulta_categoria = (select(Categoria).options(selectinload(Categoria.platos))
+                          .where(Categoria.id == categoria_id,
+                                 Categoria.restaurante_id == current_user.restaurante_id))
+
+    if current_user.rol == RolesValidos.MESERO:
+        consulta_categoria = consulta_categoria.where(Categoria.activo == True)
+
+    categoria_obj = db.exec(consulta_categoria).first()
+
+    if categoria_obj is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Categoria no encontrada")
+
+    return categoria_obj
+
+@router.patch("/{categoria_id}", response_model=CategoriaRead)
+def modificar_categoria(categoria_id: int,
+                        categoria_in: CategoriaUpdate,
+                        current_user: TokenData = Depends(VerificarRol([RolesValidos.DUENO])),
+                        db: Session = Depends(get_session)):
+    consulta_categoria = (select(Categoria)
+                          .where(Categoria.id == categoria_id,
+                                 Categoria.restaurante_id == current_user.restaurante_id))
+
+    categoria_obj = db.exec(consulta_categoria).first()
+
+    if categoria_obj is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Categoria no encontrada"
+        )
+
+    datos_actualizar = categoria_in.model_dump(exclude_unset=True)
+
+    if "nombre" in datos_actualizar:
+        consulta_categoria_rep = (select(Categoria)
+                                  .where(Categoria.id != categoria_obj.id,
+                                         Categoria.nombre == categoria_in.nombre,
+                                         Categoria.restaurante_id == current_user.restaurante_id))
+
+        if db.exec(consulta_categoria_rep).first() is not None:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail="Categoria ya existente con este nombre")
+
+    categoria_obj.sqlmodel_update(datos_actualizar)
+
+    try:
+        db.add(categoria_obj)
+        db.commit()
+        CACHE_CATEGORIAS.pop((current_user.restaurante_id, True), None)
+        CACHE_CATEGORIAS.pop((current_user.restaurante_id, False), None)
+        db.refresh(categoria_obj)
+        return categoria_obj
+
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error interno al guardar en la base de datos")
+
+@router.delete("/{categoria_id}", status_code=status.HTTP_200_OK)
+def eliminar_categoria(categoria_id: int,
+                       current_user: TokenData = Depends(VerificarRol([RolesValidos.DUENO])),
+                       db: Session = Depends(get_session)):
+    consulta_categoria = (select(Categoria)
+                          .where(Categoria.id == categoria_id,
+                                 Categoria.restaurante_id == current_user.restaurante_id))
+
+    categoria_obj = db.exec(consulta_categoria).first()
+
+    if categoria_obj is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Categoria no encontrada")
+
+    if not categoria_obj.activo :
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Categoria ya desactivada")
+
+    categoria_obj.activo = False
+
+    try:
+        db.add(categoria_obj)
+        db.commit()
+
+        CACHE_CATEGORIAS.pop((current_user.restaurante_id, True), None)
+        CACHE_CATEGORIAS.pop((current_user.restaurante_id, False), None)
+
+        return {"message": "Categoria eliminada correctamente"}
+
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error interno al guardar en la base de datos")
--- backend/app/models/categoria.py
+++ backend/app/models/categoria.py
@@ -7,33 +7,46 @@ if TYPE_CHECKING:
     from app.models.restaurante import Restaurante
     from app.models.plato import Plato
 
-class Categoria(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True, index=True)
+class CategoriaBase(SQLModel):
     nombre: str = Field(nullable=False, max_length=50)
+    activo: bool = Field(default=True, nullable=False)
 
-    restaurante_id: int | None = Field(default=None, foreign_key="restaurante.id")
+class Categoria(CategoriaBase, table=True):
+    id: int | None = Field(default=None, primary_key=True, index=True)
+    restaurante_id: int = Field(nullable=False, foreign_key="restaurante.id")
 
     platos: list["Plato"] = Relationship(back_populates="categoria")
     restaurante: Optional["Restaurante"] = Relationship(back_populates="categorias")
 
-class CategoriaBase(SQLModel):
+class CategoriaCreate(SQLModel):
     nombre: str = Field(nullable=False, max_length=50)
-    restaurante_id: int | None = Field(default=None)
-
-class CategoriaCreate(CategoriaBase):
     @field_validator("nombre")
     @classmethod
     def validar_nombre(cls, value: str):
-        nombre_sanitado = value.capitalize() if value.strip() else ""
-        if not nombre_sanitado:
-            raise ValueError("Error: Nombre Vacio")
-        return nombre_sanitado
+        nombre_sanitizado = value.strip()
+        if not nombre_sanitizado:
+            raise ValueError("Nombre no puede estar vacio ni contener solo espacios")
+        return nombre_sanitizado
 
 class CategoriaRead(CategoriaBase):
     id: int
+    restaurante_id: int
 
-class CategoriaWithPlatos(CategoriaBase):
-    id: int
+class CategoriaUpdate(SQLModel):
+    nombre: str | None = None
+    activo: bool | None = None
+
+    @field_validator("nombre")
+    @classmethod
+    def validar_nombre_opcional(cls, value: str | None):
+        if value is not None:
+            nombre_sanitizado = value.strip()
+            if not nombre_sanitizado:
+                raise ValueError("El nombre no puede estar vacío ni contener solo espacios")
+            return nombre_sanitizado
+        return value
+
+class CategoriaWithPlatos(CategoriaRead):
     platos: list["PlatoRead"]
 
 from app.models.plato import PlatoRead
